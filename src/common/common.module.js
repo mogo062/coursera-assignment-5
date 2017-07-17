d361987f0ef5010012f2a742a9943f30c1931b19
MONGI GUESMI <03989@biat.int>
2017-07-17 17:22:32 +0100

    5th assignment

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,6 +2,7 @@
   'use strict';
 
   angular.module('common',[])
+  .constant('ApiPath','https://mogo062-course5.herokuapp.com')
   .config(config);
 
   config.$inject=['$httpProvider'];
--- src/public/public.routes.js
+++ src/public/public.routes.js
@@ -18,6 +18,29 @@ function routeConfig ($stateProvider) {
     .state('public.home', {
       url: '/',
       templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.menu', {
+      url : '/menu',
+      templateUrl : 'src/public/menu/menu.html',
+      controller : 'MenuController',
+      controllerAs : 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService',function(MenuService){
+          return MenuService.getCategories();
+        }]
+      }
+    })
+    .state('public.menuitems', {
+      url : '/menu/{category}',
+      templateUrl : 'src/public/menu-items/menu-items.html',
+      controller : 'MenuItemsController',
+      controllerAs : 'menuItemsCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService',function($stateParams,MenuService){
+          console.log(MenuService.getMenuItems($stateParams.category));
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
     });
 }
 })();
